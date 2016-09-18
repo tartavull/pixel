90c558c344f93bb6b39a43126ad1eb9dba642138
Ignacio Tartavull <tartavull@gmail.com>
2016-09-18 16:30:38 -0400

    Bump version: 0.3.2 → 0.4.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='pixel',
-    version='0.3.2',
+    version='0.4.0',
     description="pixel",
     long_description=readme + '\n\n' + history,
     author="Ignacio Tartavull",
